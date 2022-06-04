b74e2e5d7db91188f2f266074520390de0c810bb
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-04 15:57:20 -0700

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/** 
+ *this code prints a word "Programming is like building a multilingual puzzle"
+*/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
