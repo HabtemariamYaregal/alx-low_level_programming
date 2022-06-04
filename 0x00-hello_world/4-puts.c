5c0eb8b687acbaf9f6b62f22b6689799a96d7ad8
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-04 16:02:26 -0700

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /** 
- *this code prints a word "Programming is like building a multilingual puzzle"
+ *the main function prints a word "Programming is like building a multilingual puzzle"
+ *the function returns 0 since we don't want to retunt anything.
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
