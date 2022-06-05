b6b95dc4658658cccf1fc2ed3c158bb32d2a23d0
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 01:32:20 -0700

    one of the finals

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
 */
 int main(void)
 {
-const char msg[] = "and that piece of art is useful\" 
-- Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, 1, msg, sizeof(msg));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
