6a4c28313db16e05817e98b0d6bed048726d9912
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 03:53:16 -0700

    last

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - print all single digit
+ *Return: returns 0 value
+ */
+int main(void)
+{
+int x;
+for (x = 48; x < 58; x++)
+{
+putchar(x);
+if (x != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\x');
+return (0);
+}
