09e4a088f53d95fe778e53e694fb89038735bc05
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 02:43:50 -0700

    second

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main - get the last digit of a random number
+ *Return: returns 0 value
+ */
+int main(void)
+{
+for(int x = 'a'; x<= 'z'; x++)
+{
+putchar(tolower(x));
+
+}
+putchar('\n'); 
+}
