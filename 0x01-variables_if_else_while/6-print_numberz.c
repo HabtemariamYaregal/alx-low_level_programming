9078d5fef3332987d2617b42db9110ff998035ef
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 03:30:26 -0700

    sixth

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - print all single digit
+ *Return: returns 0 value
+ */
+int main(void)
+{
+int x;
+for (x = 48; x < 58; x++)
+{
+putchar(x);
+}
+putchar("\n");
+return (0);
+}
