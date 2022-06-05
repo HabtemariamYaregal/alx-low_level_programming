d70905f6d09baab26feab20301663a4893175880
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 03:42:46 -0700

    eighth

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - print all single digit
+ *Return: returns 0 value
+ */
+int main(void)
+{
+int x;
+for (x = 48; x < 58; x++)
+{
+putchar(x);
+}
+for (x = 97; x < 102; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
