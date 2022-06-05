e8bbc509435c19f99fc86783a8f093ce180b3699
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 01:28:46 -0700

    one of the finals

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include <unistd.h> 
-#include <sys/syscall.h> 
+#include <unistd.h>
+#include <sys/syscall.h>
 /**
- *main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *main - prints "and that piece of art is useful" 
+ *- Dora Korpar, 2015-10-19"
  *Return: return a value 1
 */
-int main(void) 
-{ 
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-    syscall(SYS_write, 0, msg, sizeof(msg)); 
-    return 1; 
+int main(void)
+{
+const char msg[] = "and that piece of art is useful\" 
+- Dora Korpar, 2015-10-19\n";
+syscall(SYS_write, 1, msg, sizeof(msg));
+return (1);
 }
