ee06e43a37ef8b83d235b444dccff2066d4d2787
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 00:14:59 -0700

    fifth

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - printswith proper grammar, but the outcome is a piece of art,
- * Return : 0, nothing to return
+ * Return: 0, nothing to return
 */
 int main(void)
 {
