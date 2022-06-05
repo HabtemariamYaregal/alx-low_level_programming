bd326e4c74326c75234ff3a644a617d994e9e83c
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 03:39:07 -0700

    seventh

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - get the last digit of a random number
+ *Return: returns 0 value
+ */
+int main(void)
+{
+char x;
+for (x = 'z'; x >= 'a'; x--)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
