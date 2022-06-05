c3b151ed5a1a1e26444357b10ebaeb6330ae13cf
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 00:02:05 -0700

    fifth

---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printswith proper grammar, but the outcome is a piece of art,
+ * return : 0, nothing to return
+*/
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,/n");
+  return(0);
+}
