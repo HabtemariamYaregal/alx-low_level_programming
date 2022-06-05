d7b51c3f7cd7a02b3ffbdf57fed5ff0988859884
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-05 01:53:18 -0700

    zeroth

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,18 +3,20 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ *main - check weather a number is positive or negative 
+ */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if(n>0)
+if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else if (n<0)
+else if (n < 0)
 {
 printf("%d is negative\n", n);
 }
