e1def6464081cb3fe374b45a0896d6848f715ebf
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-07 09:59:36 -0700

    third

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _is lower - lower case and c
+*/
+int _islower(int c)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (x == 'c')
+{
+return (1);
+}
+else
+return (0);
+}
+}
+}
