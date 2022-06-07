090c43f152f626522b948658eef2c72892b9fe2a
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-07 02:02:24 -0700

    first and second

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <putchar.h>
+/**
+ *main - prints _putchar
+ *return: 0 nothing to return
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r'); 
+return (0);
+}
