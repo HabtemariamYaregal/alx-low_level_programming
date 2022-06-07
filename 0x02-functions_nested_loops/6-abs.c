07da0096ba0842afadd4a34aa22224893d60ae48
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-07 10:46:31 -0700

    sixth

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - chech sign
+ * Return: n, if number is positive
+ * -n, number is negative
+ * @n: parameter to be checked
+*/
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
