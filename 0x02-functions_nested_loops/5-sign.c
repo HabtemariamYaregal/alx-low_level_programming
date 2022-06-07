f63bccbcbed72e19c34d1441d1f39bfe91640521
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-07 10:40:46 -0700

    fifth

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,23 +4,23 @@
  * Return: 1, if number is positive
  * 0, number is 0
  * -1, numbe ris negative
- * @n: parameter to be checked 
+ * @n: parameter to be checked
 */
 int print_sign(int n)
 {
 if (n > 0)
 {
 _putchar('+');
-return 1;
+return (1);
 }
 else if (n == 0)
 {
 _putchar('0');
-return 0;
+return (0);
 }
 else
 {
 _putchar('-');
-return -1;
+return (-1);
 }
 }
