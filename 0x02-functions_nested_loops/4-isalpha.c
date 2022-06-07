4bd3415b8db3b61dbfef9b2d68914f6434c66cee
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-07 10:23:30 -0700

    fourth

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - lower case,letter or upper
+ * Return: 1, if condition is true
+	   0, otherwise
+ * @c: parameter to be checked 
+*/
+int _islower(int c)
+{
+return (c >= 'A' && c <= 'z')
+}
+
