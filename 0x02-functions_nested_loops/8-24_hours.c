284b097861b86ee672bc0916f2f9218c2a48cee3
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-07 11:21:34 -0700

    eighth

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+int x, y;
+for (x = 0; x < 23; x++)
+{
+for (y = 0; y < 60; y++)
+{
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+_putchar(':');
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+_putchar('\n');
+}
+}
+}
