706edad450577b14bf0f1afbda713b8e006bbc3a
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-07 02:37:47 -0700

    second

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,7 @@
 /**
  *main - prints lower case letter in function
  *Return: 0 nothing to return
-*/
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+ */
 void print_alphabet(void)
 {
 char x;
