16e0fefffe9ecfd7a3321ccf5056efb36010d6fc
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-07 10:27:37 -0700

    fourth

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 /**
  * _isalpha - lower case,letter or upper
  * Return: 1, if condition is true
-	   0, otherwise
+ * 0, otherwise
  * @c: parameter to be checked 
 */
 int _isalpha(int c)
 {
-  return (c >= 'A' && c <= 'z');
+return (c >= 'A' && c <= 'z');
 }
 
