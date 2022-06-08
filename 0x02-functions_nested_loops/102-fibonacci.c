57962bceb28fffb4e7ac37eeed05a588da0cb3b8
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-08 06:43:45 -0700

    azig

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-int first, second, next;
+int first, second, next, i;
 first = 1;
 second = 2;
 next = first + second;
 printf("%d, %d, ", first ,second);
-int i;
 for (i = 3; i <= 50; i++)
 {
 printf("%d, ", next);
@@ -14,4 +13,6 @@ first = second;
 second = next;
 next = first + second;
 }
+printf("\n");
+return (0);
 }
