805cd6648d37529845b5e38c9f100eb2ec917760
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-08 06:11:25 -0700

    avanced 1

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+int main(void)
 {
 int i, sum = 0;
 for (i = 0; i < 1024; i++)
@@ -8,5 +9,6 @@ if (i % 3 == 0 || i %5 == 0)
 sum = sum + i;
 }
 }
-printf("%d", sum);
+printf("%d\n", sum);
+return (0);
 }
