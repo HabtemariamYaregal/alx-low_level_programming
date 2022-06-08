177ca31f62fba63f60f1cff4ef5edf98ea6943ca
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-08 05:01:10 -0700

    tenth

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 /**
  * add - print the sum of two numbers
  * Return: the sum
+ * @n: integer 1
+ * @m: integer 2
 */
 
-int add (int n, int m)
+int add(int n, int m)
 {
 int p;
-p = n * m;
+p = n + m;
 return (p);
 }
