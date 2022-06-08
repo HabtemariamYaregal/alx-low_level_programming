ddb15a15f0b3bd9c51b39dffde16a5fc25fe4ac8
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-08 07:00:30 -0700

    advanced 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+/**
+ * main - print fibonacci
+ * Return: 0
+*/
 int main(void)
 {
 long int first, second, next, i;
 first = 1;
 second = 2;
 next = first + second;
-printf("%ld, %ld, ", first ,second);
+printf("%ld, %ld, ", first, second);
 for (i = 3; i <= 50; i++)
 {
-printf("%ld, ", next);
+if (i == 50)
+{
+printf("%d", next);
+}
+else
+{
+printf("%d, ", next);
+}
 first = second;
 second = next;
 next = first + second;
