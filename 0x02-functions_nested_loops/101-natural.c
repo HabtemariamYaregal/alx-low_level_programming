6a7c6b9c057254a5a5155739e0f2919196530afb
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-08 06:14:15 -0700

    avanced 1

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+/**
+ * main - print the sum of multiples of
+ *3 and 5
+*/
 int main(void)
 {
 int i, sum = 0;
 for (i = 0; i < 1024; i++)
 {
-if (i % 3 == 0 || i %5 == 0)
+if (i % 3 == 0 || i % 5 == 0)
 {
 sum = sum + i;
 }
