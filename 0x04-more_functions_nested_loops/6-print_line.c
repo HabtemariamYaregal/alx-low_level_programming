c8f783d3e5c8f015dc30bed3233f49fc7e34b99f
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-10 17:01:31 -0700

    sixth

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 10 times.
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n ; i++)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
