edbaa9ff2668daca20a2878ed8f29a31391ca0c3
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-10 17:27:46 -0700

    seventh

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,20 +6,20 @@
 void print_diagonal(int n)
 {
 int i, j;
+if (n > 0)
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
-if (n <= 0)
-{
+_putchar('\\');
 _putchar('\n');
+}
 }
 else
 {
-_putchar('\\');
-}
 _putchar('\n');
 }
 }
