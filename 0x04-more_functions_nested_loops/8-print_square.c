ccc2bf65cd4c96fad28614e0c084b425e439e1ac
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-10 17:33:35 -0700

    eighth

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - prints diagonal line.
+ * @size: size of square.
+ */
+void print_square(int size)
+{
+int i, j;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
