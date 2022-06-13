9bf04b7779c8ebde2b3c62351e4a61c20bdf4c9e
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-13 13:35:47 -0700

    fourth

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _rev - prints the string reverse
+ * print_rev - prints the string reverse
  * @s: string to be checked
  * Return: nothing
 */
-void _rev(char *s)
+void print_rev(char *s)
 {
 int i, length = 0;
 for (i = 0; s[i] != '\0'; i++)
