6be4a02519a36e5ec1cbbaf26a71b3732283d07a
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-13 14:31:36 -0700

    ninth

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copy from one to another
+ * @dest: value that copy
+ * @src: value to be copied
+ * Return: nothing
+*/
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
