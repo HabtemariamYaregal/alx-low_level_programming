d740375cc409c01fd4f2f1073d1735b3a6cc4b3a
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-13 13:42:12 -0700

    fifth

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - prints the string reverse
+ * @s: string to be checked
+ * Return: nothing
+*/
+void rev_string(char *s)
+{
+int i, length = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+length++;
+}
+for (i = (length - 1); i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
