b81d4558cd93a3028a8a6359bbf4fc7b30b98795
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-13 14:08:52 -0700

    seventh

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the string half
+ * @str: string to be checked
+ * Return: nothing
+*/
+void puts_half(char *str)
+{
+int length = 0, i;
+for (i = 0; str[i] != '\0'; i++)
+{
+length++;
+}
+for (i = (length / 2); str[i] != '\0'; i++)
+{
+if (length % 2 != 0)
+{
+i++;
+}
+_putchar(str[i]);
+}
+_putchar(str[i]);
+}
