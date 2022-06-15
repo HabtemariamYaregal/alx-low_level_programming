c3dd0dd1a3370ea927c00ede6122afffbdc58081
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-15 11:07:24 -0700

    sixth

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - capitalise the string
+ * @str: array to be changed
+ * Return: zero
+*/
+char *cap_string(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+{
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] = str[i + 1] - 32;
+}
+}
+}
+return (str);
+}
