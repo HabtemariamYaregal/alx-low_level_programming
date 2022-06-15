7f93fd9f90f076c310045d5b4fe98618d0b0b3c0
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-15 11:37:05 -0700

    sixth

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,19 @@ char *cap_string(char *str)
 int i;
 for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+if (str[0] >= 'a' && str[0] <= 'z')
 {
-if (str[i+1] >= 'a' && str[i+1] <= 'z')
+str[0] = str[0] - 32;
+}
+if (str[i] == ',' || str[i] == '\t' || str[i] == ' ' || str[i] == '\n' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 {
-str[i+1] = str[i+1] - 32;
+if (str[i] == '\t')
+{
+str[i] = ' ';
+}
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] = str[i + 1] - 32;
 }
 }
 }
