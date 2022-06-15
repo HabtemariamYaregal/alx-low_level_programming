734a86426b4b4b593f12f71d38f0d7baa96c294b
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-15 11:09:33 -0700

    sixth

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,9 @@ for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 {
-if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+if (str[i+1] >= 'a' && str[i+1] <= 'z')
 {
-str[i + 1] = str[i + 1] - 32;
+str[i+1] = str[i+1] - 32;
 }
 }
 }
