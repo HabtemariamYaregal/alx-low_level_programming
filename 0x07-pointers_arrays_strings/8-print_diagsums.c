45ac129d7640757f661c04d601a5ea9d5e2523aa
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-21 03:34:39 -0700

    final

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,28 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - add dizgonals of square matrix
- * @size: size of the array
+ * print_diagsums - add diagonals of square matrix
  * @a: array to be added
-*/
+ * @size: size of the array
+ */
 void print_diagsums(int *a, int size)
 {
-int i, j, sum1 = 0, sum2 = 0;
+int i, sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
-{
-if (i == j)
-{
-sum1 = sum1 + a[i][j];
-}
-}
-}
-for (i = 0; i < size; i++)
-{
-j = size - 1;
-sum2 = sum2 + a[i][j];
-j--;
+sum1 = sum1 + a[i];
+sum2 = sum2 + a[size - i - 1];
+a = a + size;
 }
 printf("%d, %d\n", sum1, sum2);
 }
