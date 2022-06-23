2624b1a0e72e8bd5f8ee991719d9bba199410100
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-23 16:43:10 -0700

    here we start

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion(char *s) - print strings 
+ * @s: a string to be printed
+*/
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+}
