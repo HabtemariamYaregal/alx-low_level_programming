a60b749983ee412a20fdecec0b41f399db91fdda
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-23 17:00:26 -0700

    first

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion(char *s) - print strings in reverse
+ * @s: a string to be printed
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+else
+{
+_putchar('\n');
+} 
+}
