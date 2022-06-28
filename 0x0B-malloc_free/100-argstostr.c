626422c0a6e2f38c7edb27bcd259c2947862ed40
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-28 07:11:12 -0700

    advanced one

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,9 +19,8 @@ for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
 l++;
-l++;
 }
-s = malloc(sizeof(char) * l);
+s = malloc(sizeof(char) * (l + 1));
 if (s == NULL)
 {
 return (NULL);
