587eab24f6528b4f62823c9063cc8b0dc466e717
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-28 07:14:15 -0700

    advanced one

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,7 +9,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-int i, j, k, l;
+int i, j, k, l = 0;
 char *s;
 if (ac == 0 || av == NULL)
 {
@@ -18,7 +18,10 @@ return (NULL);
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
+{
 l++;
+}
+l++; 
 }
 s = malloc(sizeof(char) * l + 1);
 if (s == NULL)
