a56c96ea1d654cdac5536a9e1f97e3f2e288f848
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-28 05:53:45 -0700

    start

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array of chars
+ * @size: size of the array
+ * @c: character
+ * Return: NULL, if size is 0 or fail
+ *         pointer to the array, is success 
+ */
+char *create_array(unsigned int size, char c)
+{
+char *ptr;
+unsigned int i = 0;
+if (size == 0)
+{
+return (NULL);
+}
+ptr = (char *) malloc(sizeof(char) * size);
+if (ptr == NULL)
+{
+return (0);
+}
+while (i < size)
+{
+*(ptr + i) = c;
+i++;
+}
+*(ptr + i) = '\0';
+return (ptr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+
+#endif
