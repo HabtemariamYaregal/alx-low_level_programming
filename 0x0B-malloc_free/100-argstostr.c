32f9199b31e38773bee9c6d44f9c07a6d4a42481
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-28 07:04:25 -0700

    advanced one

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *argstostr - concatenates all arguments
+ * @ac: size
+ * @av: pointer to pointer
+ */
+char *argstostr(int ac, char **av)
+{
+int i, j, k, l;
+char *s;
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+l++;
+l++;
+}
+s = malloc(sizeof(char) * (l + 1));
+if (s == NULL)
+{
+return (NULL);
+}
+k = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+s[k] = av[i][j];
+k++;
+}
+s[k] = '\n';
+k++;
+}
+return (s);
+}
