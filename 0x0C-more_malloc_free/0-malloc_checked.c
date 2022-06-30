da6500ab3813d0d561591bb3120e6a6b774c28b8
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-30 08:52:07 -0700

    start

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocate the memory using malloc
+ * @b: variable to located
+ * Return: a pointer to allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+ptr = (*int) malloc(size(int) * b);
+return(ptr); 
+free(ptr);
+}
