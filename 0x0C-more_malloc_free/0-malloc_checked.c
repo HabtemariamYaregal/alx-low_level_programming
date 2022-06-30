90c9a8fd8832c1a4437d10e964c47dcd7487865a
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-30 11:08:39 -0700

    left out

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,6 +5,7 @@
  * *malloc_checked - allocate the memory using malloc
  * @b: variable to located
  * Return: a pointer to allocated memory
+ * 98 if insufficient memory was available 
 */
 
 void *malloc_checked(unsigned int b)
