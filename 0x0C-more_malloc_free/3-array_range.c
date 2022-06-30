b2cd14409fa435ece31105da275fb05cc06e26b4
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-06-30 10:53:54 -0700

    last

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *array_range - create an array
+ * @min: starting point
+ * @max: lasting point
+ * Return: a pointer to allocated memory
+*/
+
+int *array_range(int min, int max)
+{
+int *arr, i, diff, j = 0;
+diff = max - min;
+arr = malloc(sizeof(int) * diff);
+for (i = min; i <= max; i++)
+{
+if (j < diff)
+{
+arr[j] = i;
+j++;
+} 
+}
+if (min > max)
+{
+return (NULL);
+}
+if (!arr)
+{
+return (NULL);
+}
+return (arr); 
+}
