e81dc58a64df678722376cee76f63e2db6bf97ae
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-07-04 10:57:37 -0700

    second

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,11 @@ if (d != NULL)
 {
 if (d->name == NULL)
 {
-d->name = "nil"
+d->name = "nil";
 }
 if (d->owner == NULL)
 {
-d->owner = "nil"
+d->owner = "nil";
 }
 printf("Name: %s\n", d->name);
 printf("Name: %f\n", d->age);
