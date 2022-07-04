903cdb33d1da6d239c5223feabf28db348094b66
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-07-04 10:56:12 -0700

    second

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,19 +7,18 @@
 */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
+if (d != NULL)
 {
-return;
-}
 if (d->name == NULL)
 {
-d->name = "(nil)";
+d->name = "nil"
 }
 if (d->owner == NULL)
 {
-d->owner = "(nil)";
+d->owner = "nil"
 }
 printf("Name: %s\n", d->name);
 printf("Name: %f\n", d->age);
 printf("Name: %s\n", d->owner);
+}
 }
