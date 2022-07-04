da59f2683fc481447bf5712ab97a0ca4144ef0b1
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-07-04 10:30:47 -0700

    start

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - about dog
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ * Description: Longer description
+*/
 struct dog
 {
 char *name;
