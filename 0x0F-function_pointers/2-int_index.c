ed92318b05eb42cca1bb7f6611221dc2d1a962d0
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-07-06 03:37:18 -0700

    second

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,7 +6,7 @@
  * int_index - execute a function of array
  * @array: array to be excuted
  * @cmp: a pointer to a function
- * @size: size of an array 
+ * @size: size of an array
  * Return: index
 */
 
@@ -18,7 +18,9 @@ if (array && cmp)
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
+{
 return (i);
+}
 }
 }
 return (-1);
