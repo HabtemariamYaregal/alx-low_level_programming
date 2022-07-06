99c9c9da05bda38d5d1a89f340dd81469484a044
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-07-06 02:18:38 -0700

    the first 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 if (array || size || action)
-{
+return;
 size_t i;
 for (i = 0; i < size; i++)
 {
 action(array[i]);
-}
 }
 }
