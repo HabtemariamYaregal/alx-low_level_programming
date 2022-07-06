56e914a7d442c4afa5f73a7e1187d472deafe531
HabtemariamYaregal <habsonofethiop@gmail.com>
2022-07-06 01:52:05 -0700

    the first 2

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: a pointer to a function
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name || f)
+{
+f(name);
+}
+}
