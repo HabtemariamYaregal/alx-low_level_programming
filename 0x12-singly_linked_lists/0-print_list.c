b79b3535f7276ef943e9ec944b968347ad933963
HabtemariamYaregal <habminilik@gmail.com>
2022-07-14 11:19:18 -0700

    here we start

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,15 @@ size_t print_list(const list_t *h)
 size_t counter = 0;
 while(h != NULL)
 {
-if (h -> str == NULL)
+if (h->str == NULL)
 {
 printf("[0] (nil)\n");
 }
 else
 {
-printf("[%u] %s\n", h -> len, h -> str);
+printf("[%u] %s\n", h->len, h->str);
 }
-h = h -> next;
+h = h->next;
 counter++;
 }
 return (counter);
