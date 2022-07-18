6f7942b07219c09dd37c6671275132f7706dfbcb
HabtemariamYaregal <habminilik@gmail.com>
2022-07-18 05:21:43 -0700

    commiting adds together

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * *add_nodeint - add a new node at first
+ * @head: add of linked list
+ * @n: integer to be added
+ * Return: the address of new element
+ * Description: singly linked list node structure
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *added;
+added = malloc(sizeof(listint_t));
+if (added == NULL)
+{
+return (NULL);
+}
+added->n = n;
+added->next = (*head);
+(*head) = added;
+
+return (*head);
+}
