f741516d5e264574f13cf8a379d3734e4d6858b9
HabtemariamYaregal <habminilik@gmail.com>
2022-07-19 12:16:03 -0700

    eightth

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - the sum of all datas
+ * @head: head of linked list
+ * Return: the address of newly added element
+ * Description: singly linked list node structure
+ */
+
+int sum_listint(listint_t *head)
+{
+int total = 0;
+if (head == NULL)
+{
+return (0);
+}
+while (head)
+{
+total= total + head->n;
+head = head->next;
+}
+return (total);
+}
