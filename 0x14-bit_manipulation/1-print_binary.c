ceed1acd32cf72e50371df14c1971ad0b1e7a776
HabtemariamYaregal <habminilik@gmail.com>
2022-07-21 16:28:58 -0700

    first

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be printed
+ */
+void print_binary(unsigned long int n)
+{
+int i, counter = 0;
+unsigned long int now;
+for (i = 63; i >= 0; i--)
+{
+now = n >> i;
+if (now & 1)
+{
+_putchar('1');
+counter++;
+}
+else if (counter)
+_putchar('0');
+}
+if (!counter)
+_putchar('0');
+}
